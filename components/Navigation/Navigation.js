a7d067ca80427e3aaa0ed995504099c81c2cbbe3
lukas-medallia <lukas@medallia.com>
2015-11-14 14:30:05 -0800

    add ratchet

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -4,10 +4,56 @@
  * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
  */
 
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import './Navigation.scss';
 import Link from '../Link';
 
+var ratchet = require('react-ratchet')
+
+console.log(ratchet)
+
+var Title = require('react-ratchet').Title;
+// var NavBar = require('react-ratchet').NavBar;
+
+
+class MyTitle extends React.Component {
+  render() {
+    return <new Title>Tripzzz!</Title>
+  }
+}
+
+
+// class MyNavBar extends React.Component {
+//   render() {
+//     return (
+//         <NavBar>
+//             <Title>Hello World!</Title>
+//         </NavBar>
+//     );
+//   }
+// }
+// class MyTitle extends React.Component {
+//   render() {
+//     return <Title>Tripz!</Title>
+//   }
+// }
+
+function MyNavBar(){
+  return(
+    <MyTitle />
+    // <MyTitle />
+    // <NavBar>
+    //     Hello World!
+    // </NavBar>
+  )
+}
+
+function Navigation() {
+  return (
+    <MyNavBar />
+  );
+}
+/*
 function Navigation() {
   return (
     <ul className="Navigation" role="menu">
@@ -20,5 +66,6 @@ function Navigation() {
     </ul>
   );
 }
+*/
 
 export default Navigation;
